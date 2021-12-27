b03e70f8d20cda5f0e9e13a2506ad7b9e33fe845
AlwinJacobThomas <kailath.ajt@gmail.com>
2021-12-27 20:48:38 +0530

    created 'config/collections' for adding constant for database | added getAllProducts

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,17 +1,22 @@
 var db = require("../config/connection");
+var collection = require("../config/collections");
 
 module.exports = {
     addProduct: (product, callback) => {
-        
-        
         db.get().collection("product")
             .insertOne(product)
             .then(data => {
+                console.log(data.insertedId)
                 callback(data.insertedId)
             })
             .catch(err => {
                 console.log("error:"+err)
             });
-
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
     }
 }
