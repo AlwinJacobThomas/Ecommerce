b92c3c99711a74dc2ff7b5b8b0a0b43712fd3726
AlwinJacobThomas <kailath.ajt@gmail.com>
2022-01-26 13:56:16 +0530

    added sessions |logout and login routes|video 19|session@server-cookie@client

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ var hbs = require('express-handlebars');
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 var fileUpload = require('express-fileupload')
+
 var app = express();
 var db = require('./config/connection')
-
+var session = require('express-session')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -21,14 +22,20 @@ app.engine( 'hbs', hbs.engine( {
   layoutsDir: __dirname + '/views/layouts/',
   partialsDir: __dirname + '/views/partials/'
 }));
-  
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
-
+app.use(session({
+  secret:"Key",
+  cookie:{maxAge:600000},
+  resave: false,
+  saveUninitialized: true
+}))
 db.connect((err)=>{
   if (err)
   console.log("connection error"+err)
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
-var productHelper = require('../helpers/product-helpers')
-// Retrieve
 
-
+var productHelpers = require('../helpers/product-helpers');
+var userHelpers = require('../helpers/user-helpers'); 
 
 /* GET home page. */
 
-
-
 router.get('/', function (req, res, next) {
-  productHelper.getAllProducts().then((products) => {
-    console.log(products)
-    res.render('users/products', { products, admin: false });
+  let user=req.session.user
+  //console.log(user);
+  productHelpers.getAllProducts().then((products) => {
+    res.render('users/products', {user, products, admin: false });
   })
 
 });
@@ -23,8 +22,25 @@ router.get('/signup',(req,res)=>{
   res.render('users/signup');
 });
 router.post('/signup',(req,res)=>{
-  
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response);
+  })
 })
-
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      
+      req.session.loginStatus=true
+      req.session.user=response.user
+      res.redirect('/')
+    }else{
+      res.redirect('/login')
+    }
+  })
+})
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
+})
 
 module.exports = router;
